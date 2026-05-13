e2f1b192d864b4ee942ff96ed74942c79413b6c4
Tim Nunamaker <tnunamak@gmail.com>
2026-05-13 12:05:03 -0500

    Keep streaming session store on server subpath

--- packages/remote-surface/src/index.ts
+++ packages/remote-surface/src/index.ts
@@ -15,14 +15,6 @@ export type * from "./backends/cdp/index.ts";
 export { NEKO_BACKEND_CAPABILITIES } from "./backends/neko/index.ts";
 export { CDP_BACKEND_CAPABILITIES } from "./backends/cdp/index.ts";
 export {
-  createStreamingSessionStore,
-  DEFAULT_MINT_IDEMPOTENCY_TTL_MS,
-  DEFAULT_STREAMING_SESSION_TTL_MS,
-  hashStreamingSessionToken,
-  MAX_IDEMPOTENCY_KEY_LEN,
-  StreamingSessionStoreError,
-} from "./server/index.ts";
-export {
   createDiagnosticsBuffer,
   redactDiagnosticsEvent,
 } from "./diagnostics/index.ts";
